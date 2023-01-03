1cab034d63a4e9219e8fea29150ba1a53fa8ff88
chonchanat <t.chonchanat@hotmail.com>
2023-01-03 16:38:52 +0700

    update: import MultiSelect to ActivityForm

--- src/components/Form/ActivityForm.js
+++ src/components/Form/ActivityForm.js
@@ -2,15 +2,23 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Button, ButtonSubmit } from '../Button';
+import { MultiSelect, MultiSelectBody } from '../MultiSelect';
+import Wrapper from '../Wrapper';
 
 import { AiOutlinePicture } from 'react-icons/ai';
+import { MdClose } from 'react-icons/md'
 
 import { postActivityAPI } from '../../api/ActivityAPI';
 
 function ActivityForm({ setPage }) {
 
     const activityReducer = useSelector(state => state.activityReducer);
-    console.log(activityReducer)
+
+    const [state, setState] = useState({
+        multiSearch: false,
+    });
+    const [search, setSearch] = useState("");
+    const [multiSelect, setMultiSearch] = useState([]);
 
     const [form, setForm] = useState({
         name: "",
@@ -18,6 +26,7 @@ function ActivityForm({ setPage }) {
         duration: "",
         waitingTime: "",
         star: "",
+        near: [],
     });
 
     function handlerSubmit(event) {
@@ -25,54 +34,76 @@ function ActivityForm({ setPage }) {
         postActivityAPI(form);
     }
 
+    function handlerClick(data) {
+        console.log(data)
+        const found = multiSelect.find(e => e.code === data.code);
+        if (!found) {
+            setMultiSearch([...multiSelect, data])
+        }
+        setState({ ...state, multiSearch: false });
+        setSearch("");
+    };
+
     return (
-        <div className="flex justify-center">
+        <div>
+            <Wrapper state={state.multiSearch} click={() => setState({ ...state, multiSearch: false })} />
             <form onSubmit={handlerSubmit}>
-                <div className="flex flex-wrap">
-                    <div className="w-[200px] flex justify-center items-center mr-20">
-                        <AiOutlinePicture size="200px" />
+                <div className="flex justify-around">
+                    <div className="w-fit flex justify-center">
+                        <AiOutlinePicture size="300px" />
                     </div>
-                    <div className="">
-                        <label className="w-[550px] flex justify-between items-center mb-4">ชื่อกิจกรรม
+                    <div className="w-[550px]">
+                        <p className="flex justify-between items-center mb-4">ชื่อกิจกรรม
                             <input type="tel" className="w-[400px] h-[36px] border-black rounded-md border px-6"
                                 onChange={(e) => setForm({ ...form, name: e.target.value })} />
-                        </label>
-                        <label className="w-[550px] flex justify-between items-center mb-4">จำนวนผู้เข้าร่วม
-                            <div className="flex">
+                        </p>
+                        <p className="flex justify-between items-center mb-4">จำนวนผู้เข้าร่วม
+                            <p className="flex">
                                 <input type="number" className="w-[320px] h-[36px] border-black rounded-md border px-6"
                                     onChange={(e) => setForm({ ...form, size: e.target.value })} />
                                 <p className="w-20 flex justify-end items-center">คน/รอบ</p>
-                            </div>
-                        </label>
-                        <label className="w-[550px] flex justify-between items-center mb-4">ระยะเวลาเล่น
-                            <div className="flex">
+                            </p>
+                        </p>
+                        <p className="flex justify-between items-center mb-4">ระยะเวลาเล่น
+                            <p className="flex">
                                 <input type="number" className="w-[320px] h-[36px] border-black rounded-md border px-6"
                                     onChange={(e) => setForm({ ...form, duration: e.target.value })} />
                                 <p className="w-20 flex justify-end items-center">นาที/รอบ</p>
-                            </div>
-                        </label>
-                        <label className="w-[550px] flex justify-between items-center mb-4">ระยะเวลารอ
-                            <div className="flex">
+                            </p>
+                        </p>
+                        <p className="flex justify-between items-center mb-4">ระยะเวลารอ
+                            <p className="flex">
                                 <input type="number" className="w-[320px] h-[36px] border-black rounded-md border px-6"
                                     onChange={(e) => setForm({ ...form, waitingTime: e.target.value })} />
                                 <p className="w-20 flex justify-end items-center">นาที</p>
-                            </div>
-                        </label>
-                        <label className="w-[550px] flex justify-between items-center mb-4">จำนวนดาว
-                            <div className="flex">
+                            </p>
+                        </p>
+                        <p className="flex justify-between items-center mb-4">จำนวนดาว
+                            <p className="flex">
                                 <input type="number" className="w-[320px] h-[36px] border-black rounded-md border px-6"
                                     onChange={(e) => setForm({ ...form, star: e.target.value })} />
                                 <p className="w-20 flex justify-end items-center">ดวง/คน</p>
-                            </div>
-                        </label>
+                            </p>
+                        </p>
+
+                        <div className="flex justify-between min-h-[100px]">
+                            <p>กิจกรรมใกล้เคียง</p>
+                            <MultiSelect>
+                                <input type="text" className="w-[400px] h-[36px] border-black rounded-md border px-6"
+                                    onChange={(e) => setSearch(e.target.value)}
+                                    value={search}
+                                    onClick={() => setState({ ...state, multiSearch: true })} />
+                                <MultiSelectBody state={state} data={activityReducer} search={search} click={handlerClick} />
+                                <div className="w-[400px] flex flex-wrap mt-4">
+                                    {multiSelect.map((data) =>
+                                        <p key={data.code} className="flex items-center p-2 mr-2 mb-2 bg-yellow rounded-3xl">{data.name}
+                                            <MdClose className="text-white ml-1" onClick={() => setMultiSearch(multiSelect.filter(e => e.code !== data.code))} />
+                                        </p>)}
+                                </div>
+                            </MultiSelect>
+                        </div>
                     </div>
-                    {/* {
-                        activityReducer.map((data) => (
-                            <div key={data.code}>
-                                <p>{data.name}</p>
-                            </div>
-                        ))
-                    } */}
+
                 </div>
 
                 <div className="flex justify-center mt-10">
---
+++ src/components/MultiSelect.js
@@ -0,0 +1,30 @@
+function MultiSelect({ children }) {
+    return (
+        <div className="relative w-fit z-50">
+            {children}
+        </div>
+    );
+}
+
+function MultiSelectBody({ state, data, search, click }) {
+    return (
+        <div className={`absolute top-[40px] left-0 right-0 w-full bg-white border-2 border-[#E0E0E0] rounded-md shadow-xl ${!state.multiSearch && "hidden"}`}>
+            {
+                data
+                    .filter((data) => {
+                        return search.toLowerCase() === ""
+                            ? data
+                            : data.name.toLowerCase().includes(search);
+                    })
+                    .slice(0, 5)
+                    .map((data) => (
+                        <p key={data.code} className="py-2 px-6 hover:bg-[#F4F4F4]"
+                            onClick={() => click(data)}>{data.name}</p>
+                    ))
+            }
+        </div>
+    )
+
+}
+
+export { MultiSelect, MultiSelectBody };
