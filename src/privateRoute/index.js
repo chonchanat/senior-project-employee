4e8519dcc3995d359cee245506916a2e48d30c0b
chonchanat <t.chonchanat@hotmail.com>
2023-01-21 17:01:48 +0700

    fix: fix bug redirect to /staff-activity when refresh browswer

--- src/privateRoute/index.js
+++ src/privateRoute/index.js
@@ -1,12 +1,24 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
+
+import Cookies from 'js-cookie';
+import { useEffect } from "react";
+import { setAuth } from "../actions/authActions";
 
 function PrivateRoute({ children }) {
 
+    const dispatch = useDispatch();
     const reducers = useSelector(state => state);
     // console.log(reducers);
+    const userCookie = Cookies.get("userCookie");
 
-    return reducers.authReducer ? children : <Navigate to="/signin"/>;
+    useEffect(() => {
+        dispatch(setAuth(JSON.parse(userCookie)));
+    }, [userCookie, dispatch]);
+
+    if (reducers.authReducer) {
+        return userCookie !== undefined ? children : <Navigate to="/signin" />;
+    }
 }
 
 export default PrivateRoute;
