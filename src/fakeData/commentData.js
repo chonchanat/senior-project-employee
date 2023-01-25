e2c1f088550a4d791c457d3fd00e3465b90488dd
chonchanat <t.chonchanat@hotmail.com>
2023-01-25 13:20:07 +0700

    create: RatingPercent and RatingTotal

---
+++ src/fakeData/commentData.js
@@ -0,0 +1,12 @@
+const rollerCoaster = {
+    total: 100,
+    average: 3.4,
+    five: 70,
+    four: 18,
+    three: 6,
+    two: 4,
+    one: 2,
+    list: [70, 18, 6, 4, 2],
+}
+
+export default rollerCoaster;
--- src/pages/test.js
+++ src/pages/test.js
@@ -5,6 +5,10 @@ import Wrapper from '../components/Wrapper';
 
 import { MdClose } from 'react-icons/md'
 
+import rollerCoaster from '../fakeData/commentData';
+
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
 function Test() {
 
     const data = activityData
@@ -62,7 +66,7 @@ function Test() {
                     <div className="flex">
                         {multiSelect.map((data) =>
                             <p key={data.code} className="flex items-center p-2 mr-2 bg-yellow rounded-3xl">{data.name}
-                                <MdClose className="text-white ml-1" onClick={() => setMultiSearch(multiSelect.filter(e => e.code !== data.code))}/>
+                                <MdClose className="text-white ml-1" onClick={() => setMultiSearch(multiSelect.filter(e => e.code !== data.code))} />
                             </p>)}
                     </div>
                 </div>
@@ -70,8 +74,61 @@ function Test() {
 
             <input onChange={(e) => setNumber(e.target.value)}></input>
             <p onClick={() => console.log(number)}>click</p>
+
+            <p className="border-b-2 my-6"></p>
+
+            <div className="flex justify-around items-center">
+                <RatingTotal />
+                <RatingPercent />
+            </div>
+
         </div>
     )
+}
+
+function RatingPercent() {
+    return (
+        <div>
+            {rollerCoaster.list.map((item, index) => (
+                <div key={index} className="flex items-center text-sm my-2">
+                    <p className="text-fha font-bold">{5 - index} star</p>
+                    <div className="w-[200px] h-[12px] bg-[#F7F7F7] mx-2 rounded-lg overflow-hidden">
+                        <div className={`bg-yellow h-[12px]`} style={{ width: item ? item * 200 / rollerCoaster.total : "0px" }} />
+                    </div>
+                    <p>{item * 100 / rollerCoaster.total} %</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+
+function RatingTotal() {
+    return (
+        <div className="w-fit text-center">
+            <p className="text-lg">Customer reviews</p>
+            <div className="flex items-center bg-[#F7F7F7] p-2 my-2 rounded-xl">
+                <StarRating rating={rollerCoaster.average} />
+                <p className="text-sm ml-2">{rollerCoaster.average} out of 5</p>
+            </div>
+            <p className="text-sm">{rollerCoaster.total} customer ratings</p>
+        </div>
+    );
+}
+
+function StarRating({ rating }) {
+    return (
+        <div className="flex">
+            {[...Array(5)].map((star, index) => {
+                return (
+                    <div key={index}>
+                        <AiFillStar className={`text-yellow text-3xl ${index >= rating && "hidden"}`} />
+                        <AiOutlineStar className={`text-yellow text-3xl ${index < rating && "hidden"}`} />
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default Test;
